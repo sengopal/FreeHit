539754cc9cec159daa4941fe6c701e8029b62408
aseef <aseef17@gmail.com>
2017-10-01 04:43:09 +0530

    fixed ExpandableListView not working with scrollview and also made ExpandableListView scroll the whole page

--- app/src/main/java/com/debut/ellipsis/freehit/Matches/ScoreCard/ScoreCardElements/ScoreCardFragment.java
+++ app/src/main/java/com/debut/ellipsis/freehit/Matches/ScoreCard/ScoreCardElements/ScoreCardFragment.java
@@ -49,6 +49,20 @@ public class ScoreCardFragment extends Fragment {
         ExpAdapterBatting = new ScoreCardExpandableListAdapterBatting(getActivity(), ExpListItemsBatting);
         ExpandListBatting.setAdapter(ExpAdapterBatting);
         setExpandableListViewHeight(ExpandListBatting, -1);
+
+
+        ExpandListBatting.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v,
+                                        int groupPosition, long id) {
+                setListViewHeight(parent, groupPosition);
+                return false;
+            }
+        });
+
+
+
         ExpandListBatting.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
 
 
@@ -90,7 +104,20 @@ public class ScoreCardFragment extends Fragment {
         ExpListItemsBowling = ScoreCardExpandableListDataPump.getDataBowling();
         ExpAdapterBowling = new ScoreCardExpandableListAdapterBowling(getActivity(), ExpListItemsBowling);
         ExpandListBowling.setAdapter(ExpAdapterBowling);
+
         setExpandableListViewHeight(ExpandListBowling, -1);
+
+        ExpandListBowling.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v,
+                                        int groupPosition, long id) {
+                setListViewHeight(parent, groupPosition);
+                return false;
+            }
+        });
+
+
         ExpandListBowling.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
 
 
@@ -132,6 +159,44 @@ public class ScoreCardFragment extends Fragment {
 
     // To control size of expandable listview based upon number of main list items
     // we'll discuss this feature after the live demo
+    private void setListViewHeight(ExpandableListView listView,
+                                   int group) {
+        ExpandableListAdapter listAdapter = (ExpandableListAdapter) listView.getExpandableListAdapter();
+        int totalHeight = 0;
+        int desiredWidth = View.MeasureSpec.makeMeasureSpec(listView.getWidth(),
+                View.MeasureSpec.EXACTLY);
+        for (int i = 0; i < listAdapter.getGroupCount(); i++) {
+            View groupItem = listAdapter.getGroupView(i, false, null, listView);
+            groupItem.measure(desiredWidth, View.MeasureSpec.UNSPECIFIED);
+
+            totalHeight += groupItem.getMeasuredHeight();
+
+            if (((listView.isGroupExpanded(i)) && (i != group))
+                    || ((!listView.isGroupExpanded(i)) && (i == group))) {
+                for (int j = 0; j < listAdapter.getChildrenCount(i); j++) {
+                    View listItem = listAdapter.getChildView(i, j, false, null,
+                            listView);
+                    listItem.measure(desiredWidth, View.MeasureSpec.UNSPECIFIED);
+
+                    totalHeight += listItem.getMeasuredHeight();
+
+                }
+            }
+        }
+
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        int height = totalHeight
+                + (listView.getDividerHeight() * (listAdapter.getGroupCount() - 1));
+        if (height < 10)
+            height = 200;
+        params.height = height;
+        listView.setLayoutParams(params);
+        listView.requestLayout();
+
+    }
+
+
+
     private void setExpandableListViewHeight(ExpandableListView listView, int group) {
         ExpandableListAdapter listAdapter = listView.getExpandableListAdapter();
         if (listAdapter == null) {
