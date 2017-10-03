5e32a9e70267a0625b7b7b5d90a021209070e2ee
jayanthg11 <jayanthg11@gmail.com>
2017-10-04 04:01:35 +0530

    checkout the career tab

--- app/src/main/java/com/debut/ellipsis/freehit/APIInterface.java
+++ app/src/main/java/com/debut/ellipsis/freehit/APIInterface.java
@@ -2,6 +2,7 @@ package com.debut.ellipsis.freehit;
 
 import com.debut.ellipsis.freehit.News.NewsArticleItem;
 import com.debut.ellipsis.freehit.News.NewsItem;
+import com.debut.ellipsis.freehit.Stats.Player.InfoItems;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -14,4 +15,6 @@ public interface APIInterface {
 
     @GET("news?")
     Call<NewsArticleItem> doGetNewsArticle(@Query("id") String id);
+    @GET("playerbio")
+    Call<InfoItems>doGetInfoResources();
 }
