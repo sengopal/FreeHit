7af709b2440052c5dbb68251ea2a0f5d59c4b449
aseef <aseef17@gmail.com>
2017-10-04 06:15:42 +0530

    Polls

--- app/src/main/java/com/debut/ellipsis/freehit/APIInterface.java
+++ app/src/main/java/com/debut/ellipsis/freehit/APIInterface.java
@@ -4,6 +4,7 @@ import com.debut.ellipsis.freehit.Matches.LiveMatches.LiveMatchCardItem;
 import com.debut.ellipsis.freehit.Matches.UpcomingMatches.UpcomingMatchCardItem;
 import com.debut.ellipsis.freehit.News.NewsArticleItem;
 import com.debut.ellipsis.freehit.News.NewsItem;
+import com.debut.ellipsis.freehit.Social.Polls.PollCardItem;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -76,6 +77,15 @@ public interface APIInterface {
 
     @GET("live")
     Call<LiveMatchCardItem> doGetLiveMatchResources();
+
+    @GET("polls")
+    Call<PollCardItem> doGetPollsListResources();
+
+    @GET("polls?")
+    Call<PollCardItem> doVotePollListResources(@Query("id") String id, @Query("cid") String cid);
+
+    @GET("polls?")
+    Call<PollCardItem> doGetSinglePollResources(@Query("id") String id);
 
 
 }
