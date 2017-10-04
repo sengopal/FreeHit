8d4c190758a84fdb4b8ebdca8d6bee08fdbba0d2
Nikhil Sridhar <snix97@gmail.com>
2017-10-05 00:10:23 +0530

    Merge branch 'master' of https://github.com/aseef17/FreeHit UTIL IS BACK BITCHESSS # Conflicts: #	app/src/main/java/com/debut/ellipsis/freehit/APIInterface.java #	app/src/main/java/com/debut/ellipsis/freehit/Matches/LiveMatches/QueryUtilLiveMatchCard.java #	app/src/main/java/com/debut/ellipsis/freehit/Matches/UpcomingMatches/UpcomingMatchCard.java #	app/src/main/java/com/debut/ellipsis/freehit/Social/Polls/PollItemAdapter.java #	app/src/main/java/com/debut/ellipsis/freehit/Social/Polls/SocialPolls.java #	app/src/main/java/com/debut/ellipsis/freehit/Stats/Player/Info_Fragment.java #	app/src/main/java/com/debut/ellipsis/freehit/Stats/Player/QueryUtilStats.java

--- app/src/main/java/com/debut/ellipsis/freehit/APIInterface.java
+++ app/src/main/java/com/debut/ellipsis/freehit/APIInterface.java
@@ -1,7 +1,12 @@
 package com.debut.ellipsis.freehit;
 
+import com.debut.ellipsis.freehit.Matches.LiveMatches.LiveMatchCardItem;
+import com.debut.ellipsis.freehit.Matches.PastMatches.PastMatchCardItem;
+import com.debut.ellipsis.freehit.Matches.UpcomingMatches.UpcomingMatchCardItem;
 import com.debut.ellipsis.freehit.News.NewsArticleItem;
 import com.debut.ellipsis.freehit.News.NewsItem;
+import com.debut.ellipsis.freehit.Social.Polls.PollCardItem;
+import com.debut.ellipsis.freehit.Stats.Player.InfoItems;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -84,16 +89,14 @@ public interface APIInterface {
     Call<PollCardItem> doVotePollListResources(@Query("id") String id, @Query("cid") String cid);
 
     @GET("polls?")
-    Call<PollCardItem> doGetSinglePollResources(@Query("id") String id);
+    Call<PollCardItem> doGetSinglePollResources(@Query("id") String id)
 
 
 
     @GET("past?max=6")
     Call<PastMatchCardItem> doGetPastCardResources();
-
     @GET("past?max=50")
     Call<PastMatchCardItem> doGetCompletePastCardResources();
-
     @GET("news?")
     Call<NewsItem> doGetNewsArticleTeam(@Query("fav") String team);
 
