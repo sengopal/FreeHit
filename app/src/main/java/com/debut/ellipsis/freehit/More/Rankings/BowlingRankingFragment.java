0f368b645043a5dcd29bade7b041e04db4a0a8b8
Nikhil Sridhar <snix97@gmail.com>
2017-10-31 02:55:51 +0530

    Added rankings

--- app/src/main/java/com/debut/ellipsis/freehit/More/Rankings/BowlingRankingFragment.java
+++ app/src/main/java/com/debut/ellipsis/freehit/More/Rankings/BowlingRankingFragment.java
@@ -3,17 +3,35 @@ package com.debut.ellipsis.freehit.More.Rankings;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.debut.ellipsis.freehit.MainActivity;
 import com.debut.ellipsis.freehit.R;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class BowlingRankingFragment extends Fragment {
 
+    private RecyclerView rv;
+    private ProgressBar mProgressBar;
+    private Spinner team_format;
+    private LinearLayoutManager mLinearLayoutManager;
 
     public BowlingRankingFragment() {
         // Required empty public constructor
@@ -24,7 +42,70 @@ public class BowlingRankingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ranking_bowling, container, false);
+        View rootView =  inflater.inflate(R.layout.fragment_ranking_batting, container, false);
+
+        View viewRecycler = rootView.findViewById(R.id.team_list);
+        rv = (RecyclerView) viewRecycler.findViewById(R.id.recycler_list);
+
+        mLinearLayoutManager = new LinearLayoutManager(getActivity());
+
+        rv.setLayoutManager(mLinearLayoutManager);
+
+        View teamSpinner = rootView.findViewById(R.id.team_format_select);
+        team_format = (Spinner) teamSpinner.findViewById(R.id.spinner);
+
+        View viewProgress = rootView.findViewById(R.id.progress);
+        mProgressBar = (ProgressBar) viewProgress.findViewById(R.id.progress_bar);
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.format_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        team_format.setAdapter(adapter);
+
+        Call<RankingItem> call = MainActivity.apiInterface.doGetRankingResources();
+        call.enqueue(new Callback<RankingItem>() {
+            @Override
+            public void onResponse(Call<RankingItem> call, Response<RankingItem> response) {
+
+                final List<RankingItem> teamList = response.body().getResults();
+                System.out.println("going into on response with size "+teamList.size());
+                mProgressBar.setVisibility(View.GONE);
+                System.out.println("TEST "+teamList.get(0).getOdi().getTeamList().get(0).getTeam());
+                rv.setAdapter(new PlayerRankingAdapter(getContext(), teamList.get(0).getOdi().getAllRounders()));
+                team_format.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+                {
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+                    {
+                        rv.setAdapter(null);
+                        final String selectedItem = parent.getItemAtPosition(position).toString();
+                        System.out.println("going here");
+                        if(selectedItem.equals("ODI"))
+                            rv.setAdapter(new PlayerRankingAdapter(getContext(), teamList.get(0).getOdi().getBowlingList()));
+                        else if(selectedItem.equals("T20"))
+                            rv.setAdapter(new PlayerRankingAdapter(getContext(), teamList.get(0).getT20().getBowlingList()));
+                        else if(selectedItem.equals("TEST"))
+                            rv.setAdapter(new PlayerRankingAdapter(getContext(), teamList.get(0).getTest().getBowlingList()));
+                    } // to close the onItemSelected
+                    public void onNothingSelected(AdapterView<?> parent)
+                    {
+
+                    }
+                });
+            }
+
+            @Override
+            public void onFailure(Call<RankingItem> call, Throwable t) {
+                mProgressBar.setVisibility(View.INVISIBLE);
+                Toast toast=Toast.makeText(getContext(),R.string.no_internet_connection,Toast.LENGTH_SHORT);
+                toast.show();
+                call.cancel();
+            }
+        });
+
+
+        return rootView;
     }
 
 }
